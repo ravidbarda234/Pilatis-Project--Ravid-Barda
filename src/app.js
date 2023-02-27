b9b8abb858bece144fea77a7a794a8470588f9c5
ravidbarda <bardaravid@gmail.com>
2023-02-27 11:16:29 +0200

    level 2

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import PAGES from "./models/pageModel.js";
 import { onChangePage } from "./routes/router.js";
 import { HOME_PAGE_LINK, ABOUT_PAGE_LINK, GALLARY_PAGE_LINK, LESSON_PAGE_LINK, CONTACT_PAGE_LINK, LOGIN_PAGE_LINK, SIGN_PAGE_LINK, ERROR_PAGE_LINK } from "./services/domService.js";
 
+
 HOME_PAGE_LINK.addEventListener("click", () => onChangePage(PAGES.HOME));
 ABOUT_PAGE_LINK.addEventListener("click", () => onChangePage(PAGES.ABOUT));
 GALLARY_PAGE_LINK.addEventListener("click", () => onChangePage(PAGES.GALLARY));
@@ -9,3 +10,16 @@ LESSON_PAGE_LINK.addEventListener("click",()=> onChangePage(PAGES.LESSON ))
 CONTACT_PAGE_LINK.addEventListener("click",()=> onChangePage(PAGES.CONTACT ))
 LOGIN_PAGE_LINK.addEventListener("click", () => onChangePage(PAGES.LOGIN));
 SIGN_PAGE_LINK.addEventListener("click", () => onChangePage(PAGES.SIGN));
+
+
+// **** nav bar scroll ****//
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
